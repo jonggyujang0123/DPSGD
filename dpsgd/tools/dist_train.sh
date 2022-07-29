8b911cd4ceabcf01f1ab3c9bbfb818464192de1c
Jonggyu Jang <jgjang0123@gmail.com>
2022-07-30 02:30:42 +0900

    multi-node-update

--- dpsgd/tools/dist_train.sh
+++ dpsgd/tools/dist_train.sh
@@ -3,12 +3,14 @@ CONFIG=$1
 GPUS=$2
 RESUME=${RESUME:-0}
 NNODES=${NNODES:-1}
-MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1:29500"}
+NODE_RANK=${NODE_RANK:-0}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+MASTER_PORT=${MASTER_PORT:-29500}
 torchrun --nnodes=$NNODES \
-	--max_restarts=3 \
+	--node_rank=$NODE_RANK \
 	--nproc_per_node=$GPUS \
-	--rdzv_backend=c10d \
-	--rdzv_endpoint=$MASTER_ADDR \
+	--master_addr=$MASTER_ADDR \
+	--master_port=$MASTER_PORT \
 	tools/main.py \
 	--config $CONFIG \
 	--multigpu True
