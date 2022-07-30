1beeea073ca87c79953c9cb799d054f2691de29d
Jonggyu Jang <jgjang0123@gmail.com>
2022-07-30 23:22:31 +0900

    final version

--- dpsgd/tools/main.py
+++ dpsgd/tools/main.py
@@ -1,6 +1,4 @@
 """========Default import===="""
-#from __future__ import absolute_import, division, print_function
-#import logging
 import argparse
 import os
 from utils.base_utils import set_random_seeds, get_accuracy, AverageMeter, WarmupCosineSchedule, WarmupLinearSchedule
@@ -21,8 +19,6 @@ from models.resnet50 import R_50_MNIST as resnet
 
 """ ===========END=========== """
 
-#logger = logging.getLogger("dpsgd")
-
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class = argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--config",
@@ -33,11 +29,12 @@ def parse_args():
             help="Local rank. Necessary for using the torch.distributed.launch utility.",
             default= False)
     parser.add_argument("--resume",
+            type=int,
             default= 0,
             help="Resume the last training from saved checkpoint.")
     parser.add_argument("--test", 
-            type=bool,
-            default = False,
+            type=int,
+            default = 0,
             help="if test, choose True.")
     args = parser.parse_args()
     return args
@@ -52,7 +49,7 @@ def get_data_loader(cfg, args):
 
 
 
-def load_ckpt(checkpoint_fpath, map_location,is_best =False):
+def load_ckpt(checkpoint_fpath, is_best =False):
     """
     Latest checkpoint loader
         checkpoint_fpath : 
@@ -65,12 +62,12 @@ def load_ckpt(checkpoint_fpath, map_location,is_best =False):
     example :
     """
     if is_best:
-        ckpt_path = checkpoint_fpath+'/'+'checkpoint.pt'
-    else:
         ckpt_path = checkpoint_fpath+'/'+'best.pt'
+    else:
+        ckpt_path = checkpoint_fpath+'/'+'checkpoint.pt'
     try:
         print(f"Loading checkpoint '{ckpt_path}'")
-        checkpoint = torch.load(checkpoint_fpath, map_location=map_location)
+        checkpoint = torch.load(ckpt_path)
     except:
         print(f"No checkpoint exists from '{ckpt_path}'. Skipping...")
         print("**First time to train**")
@@ -103,11 +100,10 @@ def save_ckpt(checkpoint_fpath, checkpoint, is_best=False):
 
 def train(wandb, args, cfg, ddp_model):
     ## Setting 
-
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=cfg.lr, momentum=0.9, weight_decay=cfg.weight_decay)
     scaler = torch.cuda.amp.GradScaler(enabled=cfg.use_amp)
-    train_loader, test_loader = get_data_loader(cfg=cfg, args=args)
+    train_loader, test_loader, _ = get_data_loader(cfg=cfg, args=args)
     if cfg.decay_type == "cosine":
         scheduler = WarmupCosineSchedule(optimizer, warmup_steps=cfg.warmup_steps, t_total=cfg.epochs*len(train_loader))
     else:
@@ -116,24 +112,18 @@ def train(wandb, args, cfg, ddp_model):
     AvgLoss = AverageMeter()
     # We only save the model who uses device "cuda:0"
     # To resume the device for the save model woule be also "cuda:0"
-    if args.resume == True:
-        if args.multigpu:
-            map_location = {"cuda:0": "cuda:{}".format(args.local_rank)}
-        else:
-            map_location = {"cuda:0": "cuda:0"}
-        ckpt = load_ckpt(cfg.ckpt_fpath, map_location)
-        ddp_model.load_state_dict(ckpt['model'])
+    if args.resume:
+        ckpt = load_ckpt(cfg.ckpt_fpath)
         optimizer.load_state_dict(ckpt['optimizer'])
         scheduler.load_state_dict(ckpt['scheduler'])
-        start_epoch = ckpt['epoch']
+        start_epoch = ckpt['epoch']+1
 
         
 
     # Prepare dataset and dataloader
-
     for epoch in range(start_epoch, cfg.epochs):
-        if args.local_rank not in [-1,1]:
-            print("Local Rank: {}, Epoch: {}, Training ...".format(args.local_rank, epoch))
+#        if args.local_rank not in [-1,1]:
+#            print("Local Rank: {}, Epoch: {}, Training ...".format(args.local_rank, epoch))
 
         ddp_model.train()
         tepoch = tqdm(train_loader, 
@@ -147,39 +137,39 @@ def train(wandb, args, cfg, ddp_model):
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
-            scheduler.step()
-            #optimizer.step()
+            scheduler.step() #optimizer.step()
             AvgLoss.update(loss.item(), n =len(data))
             if args.local_rank in [-1,0]:
                 tepoch.set_description(f'Epoch {epoch}: train_loss: {AvgLoss.avg:2.2f}, lr : {scheduler.get_lr()[0]:.2E}')
 
         # save and evaluate model routinely.
         if epoch % cfg.interval_val == 0:
+            val_acc = evaluate(args, model=ddp_model, device=cfg.device, val_loader=test_loader, use_amp = cfg.use_amp)
             if args.local_rank in [-1, 0]:
-                val_acc = evaluate(model=ddp_model, device=cfg.device, val_loader=test_loader, use_amp = cfg.use_amp)
+                model_to_save = ddp_model.module if hasattr(ddp_model, 'module') else ddp_model
                 ckpt = {
-                        'model': ddp_model.state_dict(),
+                        'model': model_to_save.state_dict(),
                         'optimizer': optimizer.state_dict(),
                         'scheduler': scheduler.state_dict(),
                         'epoch': epoch
                         }
                 save_ckpt(checkpoint_fpath=cfg.ckpt_fpath, checkpoint=ckpt)
-                if best_acc < val_acc.avg:
+                if best_acc < val_acc:
                     save_ckpt(checkpoint_fpath=cfg.ckpt_fpath, checkpoint=ckpt, is_best=True)
-                    best_acc = val_acc.avg
+                    best_acc = val_acc
                 if cfg.wandb.active:
                     wandb.log({
                         "loss": AvgLoss.val,
-                        "val_acc" : val_acc.avg
+                        "val_acc" : val_acc
                         })
                 print("-"*75+ "\n")
-                print(f"| {epoch}-th epoch, training loss is {AvgLoss.avg}, and Val Accuracy is {val_acc.avg} = {val_acc.sum}/{val_acc.count}\n")
+                print(f"| {epoch}-th epoch, training loss is {AvgLoss.avg}, and Val Accuracy is {val_acc*100:2.2f}%\n")
                 print("-"*75+ "\n")
         AvgLoss.reset()
         tepoch.close()
 
 
-def evaluate(model, val_loader, device, use_amp):
+def evaluate(args, model, val_loader, device, use_amp):
     model.eval()
     acc = AverageMeter()
     valepoch = tqdm(val_loader, 
@@ -196,40 +186,46 @@ def evaluate(model, val_loader, device, use_amp):
             acc.update(acc_batch, n=inputs.size(0))
             valepoch.set_description(f'Validation Acc: {acc.avg:2.2f}')
     valepoch.close()
-    return acc
+    result  = torch.tensor([acc.sum,acc.count]).to(device)
+    if args.multigpu:
+        torch.distributed.barrier()
+#        torch.distributed.all_reduce(result, op =torch.distributed.ReduceOp.SUM)
+        torch.distributed.reduce(result, op =torch.distributed.ReduceOp.SUM, dst=0)
+
+    return result[0].cpu().item()/result[1].cpu().item()
 
 def test(args, cfg, model):
-    _, test_loader = get_data_loader(cfg=cfg, args= args)
+    _, _, test_loader = get_data_loader(cfg=cfg, args= args)
     print("Loading checkpoints ... ")
-    map_location = {"cuda:0": "cuda:{}".format(args.local_rank)}
-    ckpt = load_ckpt(checkpoint_fpath= cfg.fpath, map_location = map_location, is_best = True)
-    model.load_state_dict(ckpt['model'])
     model.eval()
     acc = AverageMeter()
     #example_images = []
+    correct_list = []
     with torch.no_grad():
-        if args.local_rank in [-1,0]:
-            tepoch = tqdm(test_loader, unit= " batch")
-            tepoch.set_description("TEST")
-        else:
-            tepoch = tqdm(test_loader, unit= " batch", disable=True)
+        tepoch = tqdm(test_loader, 
+                     unit= " batch", 
+                     disable = args.local_rank not in [-1,0]
+                     )
         for batch, (inputs, labels) in enumerate(tepoch):
             inputs = inputs.to(cfg.device, non_blocking=True)
             # compute the output
             with torch.cuda.amp.autocast(enabled=cfg.use_amp):
-                output = model(inputs).detach().cpu()
-
-            # Measure accuracy
+                output = model(inputs).cpu().detach()
             acc_batch = get_accuracy(output=output, label=labels)
             acc.update(acc_batch, n=inputs.size(0))
-            #example_images.append(wandb.Image(data[0], caption="Pred: {} Truth: {}".format(pred[0].detach().item(), target[0])))
-
+            #example_images.append(wandb.Image(data[0], caption="Pred: {} Truth: {}".format(pred[0].detach().item(), target[0])
         #wandb.log({"Examples":example_images})
+    result  = torch.tensor([acc.sum,acc.count]).to(cfg.device)
+    if args.multigpu:
+        torch.distributed.barrier()
+#        torch.distributed.all_reduce(result, op =torch.distributed.ReduceOp.SUM)
+        torch.distributed.reduce(result, op =torch.distributed.ReduceOp.SUM, dst=0)
+
+    if args.local_rank in [-1,0]:
         print("-"*75+ "\n")
-        print(f"| Testset accuracy is {acc.avg} = {acc.sum}/{acc.count}\n")
+        print(f"| Testset accuracy is {result[0].cpu().item()/result[1].cpu().item()} = {result[0].cpu().item()}/{result[1].cpu().item()}\n")
         print("-"*75+ "\n")
 
-    return acc
 
 
 def main():
@@ -271,17 +267,21 @@ def main():
     model = resnet(num_classes=cfg.num_classes)
     # if custom_pre-trained model : model.load_from(np.load(<path>))
     model = model.to(cfg.device)
+    if args.resume or args.test:
+        ckpt = load_ckpt(cfg.ckpt_fpath, is_best = args.test)
+        model.load_state_dict(ckpt['model'])
+
     pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f'Number of trainable parameter is {pytorch_total_params:.2E}')
     if args.multigpu:
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank], output_device=args.local_rank)
 
 
+
     if args.test:
         test(args, cfg, model)
     else:
         train(wandb, args, cfg, model)
-    
 
 
 if __name__ == '__main__':
